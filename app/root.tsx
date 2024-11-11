e98342de12e18cb7b46b220d64519b424876bfc6
tomlit <tomlit@gmail.com>
2024-11-11 17:23:30 -0500

    dockerignroe, fix root

---
+++ app/root.tsx
@@ -0,0 +1,11 @@
+export const links = () => [
+  {
+    rel: "preconnect",
+    href: "https://fonts.googleapis.com",
+  },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+]; 
