bdf3b0f601f2a2e1df02316c6d84ef91b9842105
tomlit <tomlit@gmail.com>
2024-11-20 00:08:05 -0500

    cssfix

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,52 +1,86 @@
 import {
+  Link,
   Outlet,
+  ReactNode,
   ScrollRestoration,
-  createRootRoute,
+  createRootRouteWithContext,
 } from "@tanstack/react-router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Meta, Scripts } from "@tanstack/start";
-import { ClerkProvider } from "@clerk/tanstack-start";
-import { useThemeStore } from "@/store/themeStore";
-import { dark } from "@clerk/themes";
-import { seo } from "@/utils/seo";
+import * as React from "react";
+import type { QueryClient } from "@tanstack/react-query";
 import { AccessControl } from "@/components/AccessControl";
+
 import { DefaultCatchBoundary } from "@/components/DefaultCatchBoundary";
 import { NotFound } from "@/components/NotFound";
-import type { ReactNode } from "react";
-
-// Import CSS files
-import "@/styles/app.css?url";
-import "@/styles/gamepad.css?url";
-
-export const Route = createRootRoute({
-  head: () => {
-    return {
-      links: [
-        {
-          rel: "icon",
-          href: "/favicon.ico",
-        },
-      ],
-      scripts: import.meta.env.PROD
-        ? []
-        : [
-            {
-              type: "module",
-              children: /* js */ `
-          import RefreshRuntime from "/_build/@react-refresh"
-          RefreshRuntime.injectIntoGlobalHook(window)
-          window.$RefreshReg$ = () => {}
-          window.$RefreshSig$ = () => (type) => type
-        `,
-            },
-          ],
-    };
-  },
+import appCss from "@/styles/app.css?url";
+import gamepadCss from "@/styles/gamepad.css?url";
+import { dark } from "@clerk/themes";
+import { seo } from "@/utils/seo";
+import { ClerkProvider } from "@clerk/tanstack-start";
+import { useThemeStore } from "@/store/themeStore";
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      ...seo({
+        title: "",
+        description: ``,
+      }),
+    ],
+    links: [
+      { rel: "stylesheet", href: appCss },
+      { rel: "stylesheet", href: gamepadCss },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+      { rel: "icon", href: "/favicon.ico" },
+    ],
+    scripts: import.meta.env.PROD
+      ? []
+      : [
+          {
+            type: "module",
+            children: /* js */ `
+      import RefreshRuntime from "/_build/@react-refresh"
+      RefreshRuntime.injectIntoGlobalHook(window)
+      window.$RefreshReg$ = () => {}
+      window.$RefreshSig$ = () => (type) => type
+    `,
+          },
+        ],
+  }),
 
   beforeLoad: async () => {
     return {};
   },
-
   errorComponent: DefaultCatchBoundary,
+
+  notFoundComponent: () => <NotFound />,
   component: RootComponent,
 });
 
@@ -87,13 +121,13 @@ function RootDocument({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
       </head>
       <body className="min-h-screen font-sofia antialiased">
         {children}
         <ScrollRestoration />
+        <TanStackRouterDevtools position="bottom-right" />
+        <ReactQueryDevtools buttonPosition="bottom-left" />
         <Scripts />
       </body>
     </html>
